e27a004f31a55d39aa10457194b9f73bc23b7b2a
Dimitris Charalampopoulos <up1083802@ac.upatras.gr>
2024-02-26 12:01:35 +0200

    cookies commit

--- ex0.py
+++ ex0.py
@@ -10,8 +10,22 @@ def more(text):
             if reply == 'n':
                 break
 
-url = 'http://python.org/'   # προσδιορισμός του url
+url = 'http://eclass.upatras.gr/'   # προσδιορισμός του url
 
 with requests.get(url) as response:  # το αντικείμενο response
-    html = response.text
-    more(html)
+    #html = response.text
+    #more(html)
+
+    print(f"Website headers are {url} \n , {response.headers} \n\n")
+
+    server = response.headers.get('Server')
+
+    if server:
+        print(f"The server is {server}")
+    else:
+        print("No server found")
+
+    cookies = response.cookies.get_dict()
+
+    print(cookies)
+

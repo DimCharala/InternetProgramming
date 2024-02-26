5b2c7cdf4c2bfec54ecacb5275053ec1c0484c81
Dimitrios Charalampopoulos <74198214+DimCharala@users.noreply.github.com>
2024-02-26 13:05:42 +0200

    Add files via upload

--- ex0.py
+++ ex0.py
@@ -1,5 +1,6 @@
 import requests  # εισαγωγή της βιβλιοθήκης
 
+
 def more(text):
     count = 0
     for line in text.split('\n'):
@@ -10,28 +11,9 @@ def more(text):
             if reply == 'n':
                 break
 
-url = input("Enter a URL: ")   # προσδιορισμός του url
 
-if not url.startswith("http://"):
-    url = "http://" + url
+url = 'http://python.org/'  # προσδιορισμός του url
 
 with requests.get(url) as response:  # το αντικείμενο response
-    #html = response.text
-    #more(html)
-
-    print(f"Website headers are {url} \n , {response.headers} \n\n")
-
-    server = response.headers.get('Server')
-
-    if server:
-        print(f"The server is {server}")
-    else:
-        print("No server found")
-
-    cookies = response.headers.get("Set-Cookie")
-
-    if cookies:
-        print(f"The cookies is {cookies}")
-    else:
-        print("No cookie found")
-
+    html = response.text
+    more(html)
